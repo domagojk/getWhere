af1328282d3b55183cbf00c9360f979eeaffb41a
Domagoj <domagoj.kriskovic@gmail.com>
2015-05-04 17:00:56 +0200

    added getwhere function

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # getwhere
 Simple function for returning object value based on passed options
+
+## examples
+
+Object for extracting data
+
+```javascript
+var data = {
+  go_char_right: {
+      'keys': ['Right'],
+      'os': 'mac'
+    },
+    
+   go_char_left: {
+      'keys': ['Left'],
+      'os': {
+        'mac': {
+          'keys': ['Left', 'Ctrl-B']
+        }
+      }
+    },
+
+    go_doc_end: {
+      'keys': ['Ctrl-End'],
+      'os': {
+        'mac': {
+          'keys': ['Cmd-Down', 'Cmd-End', 'Ctrl-Down'],
+          'extension': {
+            'html': {
+              'keys': ['Ctrl-H'] 
+            },
+            'js': {
+              'keys': ['Ctrl-J'] 
+            }
+          }
+        }
+      }
+    }
+}
+```
+Examples of retrieving data
+
+```javascript
+getWhere("go_char_right", {data:data, toReturn: "keys"})
+// return ["Right"]
+
+getWhere("go_char_right", {data:data, toReturn: "keys", options: {os: "mac"} })
+// return ["Right"]
+
+getWhere("go_char_right", {data:data, toReturn: "keys", options: {os: "pc"} })
+// return false
+
+getWhere("go_char_right", {data:data, toReturn: "keys", flags: {explicit: true} })
+// return false
+
+getWhere("go_char_right", {
+  data:data, 
+    toReturn: "keys", 
+    options: {os: "mac"}, 
+    flags: {explicit: true} 
+})
+// return ["Right"]
+
+getWhere("go_char_left", {data:data, toReturn: "keys" })
+// return ["Left"]
+
+getWhere("go_char_left", {data:data, toReturn: "keys", options: {os: "mac"} })
+// return ["Left", "Ctrl-B"]
+
+getWhere("go_doc_end", {data:data, toReturn: "keys", options: {os: "mac"} })
+// return ["Cmd-Down", "Cmd-End", "Ctrl-Down"]
+
+getWhere("go_doc_end", {
+  data:data, 
+    toReturn: "keys", 
+    options: {os: "mac", extension: "html"} 
+})
+// return ["Ctrl-H"]
+```
---
+++ getwhere.js
@@ -0,0 +1,98 @@
+var getWhere = function(name, params) {
+  var options = params.options;
+  var flags = params.flags;
+  var data = params.data;
+  var toReturn = params.toReturn;
+
+  if(typeof name != 'string') {
+    throw new Error("Invalid preference name");
+  }
+
+  data = data[name];
+  
+  // if preference value is string return it
+  if(typeof data != 'object')
+    return data;
+
+  // if preference value is object and there is no options, return property
+  if(!options) {
+    if(flags && flags.explicit) {
+      var dataKeys = Object.keys(data);
+      if(dataKeys.length == 1 && dataKeys.indexOf(toReturn))
+        return data[toReturn];
+      else
+        return false;
+    }
+    return data[toReturn];
+  }
+
+  // function for checking options and retriving appropriate keys
+  var getValue = function(data) {
+    // get data and options keys
+    var dataKeys = Object.keys(data);
+    var optKeys = Object.keys(options);
+
+    if(flags && flags.explicit) {
+      //if explicit is set, return only if options keys match data keys
+      var hasOptionskeys = dataKeys.filter(function(n) {
+        // special key not in options object
+        if(n == toReturn)
+          return true;
+        // ignore if object
+        if(typeof data[n] == 'object')
+          return true;
+        // if data key exists in options
+        return optKeys.indexOf(n) != -1
+      });
+
+      if(hasOptionskeys.length != dataKeys.length)
+        return false;
+    }
+
+    // intersection between optKeys and dataKeys
+    var hasDataKeys = optKeys.filter(function(n) {
+        return dataKeys.indexOf(n) != -1
+    });
+
+    var returnKeys = true;
+
+    if(dataKeys.indexOf(toReturn) == -1) {
+      returnKeys = false;
+    }
+
+    // loop trough key filters
+    for(var i in hasDataKeys) {
+      var key = hasDataKeys[i];
+
+      if(typeof data[key] == 'string') {
+        // if key filter is string only check if it is the same as in options
+        if(data[key] != options[key]) {
+          returnKeys = false;
+          break;
+        }
+      } else if(Array.isArray(data[key])) {
+        // if key filter is array check if it contains the same as in options
+        if(data[key].indexOf(options[key]) == -1) {
+          returnKeys = false;
+          break;
+        }
+      } else if (typeof data[key] == 'object' && data[key][options[key]]) {
+        // if key filter is object, recursive call to getValue with new data
+        return getValue(data[key][options[key]]);
+      }
+    }
+
+    if(returnKeys) {
+      return data[toReturn];
+    }
+
+    return false;
+  }
+  
+  return getValue(data);
+};
+
+if (typeof exports == "object" && typeof module == "object")
+  module.exports = getWhere;
+else
+  window.getWhere = getWhere;
